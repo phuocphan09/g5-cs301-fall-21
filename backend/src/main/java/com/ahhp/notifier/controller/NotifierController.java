8c02300f753a36cce57e48de9b733b1793fbd5a4
HalfCentaurHalfHuman <68955559+HalfCentaurHalfHuman@users.noreply.github.com>
2021-12-28 10:24:06 +0700

    Updated local host from 8080 to 3000 to match front-end

--- backend/src/main/java/com/ahhp/notifier/controller/NotifierController.java
+++ backend/src/main/java/com/ahhp/notifier/controller/NotifierController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class NotifierController {
 

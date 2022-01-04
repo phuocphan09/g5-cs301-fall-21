13c1b43f33960776488331fd00c3a2ed681e46d3
HalfCentaurHalfHuman <hoang.tran.200032@student.fulbright.edu.vn>
2022-01-04 14:05:57 +0700

    Added CORS in Mailing Notifier

--- backend/src/main/java/com/ahhp/notifier/controller/MailingController.java
+++ backend/src/main/java/com/ahhp/notifier/controller/MailingController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.InvalidParameterException;
 import java.util.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class MailingController {
 
